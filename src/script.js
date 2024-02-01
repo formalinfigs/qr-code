e9be8b7be513216c3516d0ac0cc1ddb6790f1643
anastasia zaikovskaya <formalinfigs@gmail.com>
2024-02-01 23:52:26 +0300

    Improve script

--- src/script.js
+++ src/script.js
@@ -1,82 +1,35 @@
-const copyLink = async (title) => {
-  title.dispatchEvent(new MouseEvent("mouseover", { bubbles: true }));
-  const hoverItems = document.querySelectorAll("#hover-items");
-  hoverItems[hoverItems.length - 1].click();
-
-  const actions = document.querySelector("[aria-label=Действия]");
-  actions.click();
-
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve();
-    }, 500)
-  );
-  const share = document.querySelector('[test-id="share"]');
-  share.click();
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve();
-    }, 500)
-  );
-};
-
-const extractLink = async (title) => {
-  window.focus();
-
-  if (document.activeElement) {
-    document.activeElement.blur();
-  }
-
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve();
-    }, 500)
-  );
-
-  const url = await navigator.clipboard.readText();
-  const fileName = title.innerText;
-
-  return {
-    fileName,
-    url,
-  };
-};
-
-function createButton(context, func) {
-  var button = document.createElement("input");
+function createCopyButton(context, func) {
+  const button = document.createElement("input");
   button.type = "button";
-  button.value = "Скопировать ссылки на видео";
+  button.value = "Скопировать";
   button.onclick = func;
+  button.style.cursor = "pointer";
+  button.style.height = "28px";
+  button.style.margin = "auto 0 0 10px";
+
   context.appendChild(button);
 }
-
-const titleContainer = document.getElementById("page-title-container");
-window.items = [];
 
 const extractAllLinks = async () => {
   window.items = [];
 
-  const titles = Array.from(document.querySelectorAll("#video-title"));
+  const videoAnchors = Array.from(
+    document.querySelectorAll("#thumbnail-anchor")
+  );
 
-  for (const title of titles) {
-    await copyLink(title);
-    const link = await extractLink(title);
+  const links = videoAnchors.map((anchor) => {
+    const fileName = anchor.getAttribute("aria-label");
+    const videoId = anchor.getAttribute("href").split("/")[2];
+    const url = `https://youtu.be/${videoId}`;
 
-    items.push(link);
-  }
+    return {
+      fileName,
+      url,
+    };
+  });
 
-  const draft = {
-    format: "png",
-    margin: 1,
-    color: {
-      dark: "#000000ff",
-      light: "#ffffffff",
-    },
-    outputDir: "",
-    links: [],
-  };
-
-  draft.links = [...window.items];
+  window.items = links;
+  draft.links = links;
 
   await navigator.clipboard.writeText(JSON.stringify(draft));
   console.log(
@@ -84,4 +37,6 @@ const extractAllLinks = async () => {
   );
 };
 
-createButton(titleContainer, extractAllLinks);
+const titleContainer = document.getElementById("page-title-container");
+window.items = [];
+createCopyButton(titleContainer, extractAllLinks);
